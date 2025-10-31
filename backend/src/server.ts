1aa566ee71b8dd8f2ca77958f5c78e2d97deea8f
JeffoLuis9 <jeffersonrc2508@gmail.com>
2025-10-30 21:22:41 -0500

    Controller Usuario
    Se agrego el modulo de Usuarios.
    De momento solo esta completo el create estare revisando los otros metodos. PD: Ingrese en duro los tipos de eventos en la BD para que se pueda crear el usuario

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
 import express from "express"
+import usuarioRouter from './modules/usuario/usuario.controller';
 
 const app = express()
 
 app.get("/", (req, res) => {
     res.json({ message: "Hellos World" });
 });
+app.use(express.json());
+app.use('/usuarios', usuarioRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
+
 
