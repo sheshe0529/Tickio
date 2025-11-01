330019dd7a3988dd3191c89f43766223c1b01673
emely <a20216500@pucp.edu.pe>
2025-10-31 19:23:19 -0500

    compras pero solo con compras.controller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,14 @@
-
 import express from "express";
-// Importa tus routers (controllers)
-import usuarioRouter from './modules/usuario/usuario.controller';
-import compraRouter from './modules/compra/compra.router';
+import usuarioRouter from "./modules/usuario/usuario.controller";
+import compraRouter from "./modules/compra/compra.controller"; 
 
 const app = express();
-// Middleware para interpretar JSON
 app.use(express.json());
 
-// Rutas base
-app.use('/usuarios', usuarioRouter); // Todas las rutas de usuario empiezan con /usuarios
-app.use('/compras', compraRouter);   // Todas las rutas de compra empiezan con /compras
+app.use("/usuarios", usuarioRouter);
+app.use("/compras", compraRouter);
 
-
-// Ruta de prueba
-app.get("/", (_req, res) => {
-    res.json({ message: "Servidor corriendo correctamente" });
-});
+app.get("/", (_req, res) => res.json({ message: "Servidor corriendo correctamente" }));
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
-
-
-
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
